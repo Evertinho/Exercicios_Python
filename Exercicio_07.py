3a1c4ae53cd1747124b0e36767310bb9e745f11c
Evertinho <barreto.everton2020@gmail.com>
2021-09-04 03:05:56 -0300

    Média Aritmética

---
+++ Exercicio_07.py
@@ -0,0 +1,5 @@
+# Média Aritmética
+n1 = float(input('Digite a primeira nota do aluno: '))
+n2 = float(input('Digite a segunda nota do aluno: '))
+me = (n1 + n2) / 2
+print(f'A média do aluno é {me:.2f}!')
