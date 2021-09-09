8bbc4b4845f72b9e93332886163647d70d45f66f
Evertinho <barreto.everton2020@gmail.com>
2021-09-08 21:27:00 -0300

    Reajuste de Salário

---
+++ Exercicio_13.py
@@ -0,0 +1,7 @@
+# Reajuste de Salário
+sal1 = float(input('Digite seu salário: R$'))
+aum = sal1 * 0.15
+sal2 = sal1 + aum
+print(f'Salário Inicial: R${sal1:.2f}')
+print(f'Aumento de 15%: R${aum:.2f}')
+print(f'Salário Final: R${sal2:.2f}')
