c6be3ffa0ccaaf23fde9eea4053a41bea1d5411c
Everton Barreto <barreto.everton2020@gmail.com>
2021-09-11 23:34:16 -0300

    Delete Exercicio_17.py

--- Exercicio_17.py
+++
@@ -1,6 +0,0 @@
-#Catetos e Hipotenusa
-from math import hypot
-co = float(input('Comprimento do cateto oposto em cm: '))
-ca = float(input('Comprimento do cateto adjacente em cm: '))
-hip = hypot(co, ca)
-print(f'A hipotenusa é igual a \033[1;32m{hip:.2f}\033[m.')
