1dcabb69ce5cb36747ee327eac15548986192ae4
Evertinho <barreto.everton2020@gmail.com>
2021-09-20 23:58:16 -0300

    Maior e Menor Valor

---
+++ Exercicio_33.py
@@ -0,0 +1,7 @@
+# Maior e Menor Valor
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+n3 = int(input('Terceiro número: '))
+lista = [n1, n2, n3]
+print(f'O maior número digitado foi {max(lista)}.')
+print(f'O menor número digitado foi {min(lista)}.')
